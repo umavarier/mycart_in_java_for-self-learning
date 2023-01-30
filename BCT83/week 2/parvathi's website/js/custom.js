d8c78783de09018fe9a46a3e1631aa4c3c5a1a7c
Parvathi Kumar <parvathi.kumar1989@gmail.com>
2023-01-30 20:52:41 +0530

    initial commit

--- BCT83/week 2/parvathi's website/js/custom.js
+++ BCT83/week 2/parvathi's website/js/custom.js
@@ -79,3 +79,11 @@
 
   });
 
+  function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
+
